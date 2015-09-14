59714132f1ee2bf0c41d4e38b052eadf851a3f4b
Kandar <k4ndar@yahoo.com>
2015-09-14 10:30:24 +0700

    Adjust to new change

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -2,14 +2,22 @@
 
 namespace Controller;
 
-use Respect\Validation\Validator as v;
+use Panada\Resource\Controller;
 
-class Home extends \Panada\Resource\Controller
+class Home
 {
+    use Controller
+    {
+        Controller::__construct as private _construct;
+    }
+
+    public function __construct()
+    {
+        $this->_construct();
+    }
+    
     public function index()
     {
-        $this->response->setHeaders('Content-Type', 'text/html; charset=utf-8');
-        
         return $this->output('index', ['name' => 'Panada']);
     }
     
@@ -26,53 +34,5 @@ class Home extends \Panada\Resource\Controller
     public function test2()
     {
         return __METHOD__;
-    }
-    
-    public function testDB()
-    {
-        $database = \Panada\Medoo\Medoo::getInstance();
-        
-        //$name = time();
-        //
-        //$last_user_id = $database->insert("users", [
-        //    "name" => $name,
-        //    "email" => $name."@bar.com",
-        //]);
-        //
-        //var_dump($last_user_id);exit;
-        
-        $users = $database->select("users", [
-            "name",
-            "email"
-        ])->fetchAll(\PDO::FETCH_ASSOC);
-        
-        return print_r($users, true);
-    }
-    
-    public function testnotorm()
-    {
-        $db = \Panada\Notorm\NotORM::getInstance();
-        
-        foreach ($db->users() as $user) { // get all applications
-            echo $user['name'].'<br>';
-        }
-    }
-    
-    public function testRespect()
-    {
-        //new Panada\Resource\Kandar;
-        //exit;
-        $number = 123;
-        var_dump(v::numeric()->validate($number));
-    }
-    
-    public function testImagine()
-    {
-        $imagine = new \Imagine\Gd\Imagine();
-    }
-    
-    public function testMailer()
-    {
-        $mail = new \PHPMailer;
     }
 }
